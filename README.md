4148e68d23b0f04690905c75f8ecff7f46a7a42c
Hari Nathan <nathanhari@gmail.com>
2024-09-16 10:07:47 -0400

    Added README and added some variance to the time series.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SignalRecovery2024
+
+Two files:
+(1) SignalRecovery.py: this is the main code for signal recovery. To use it, import the file, and call "recover_signal." The arguments are:
+    > sig_w_gaps (required) --> the signal, as 1 dimensional numpy.array, with some values set to numpy.nan (these are the missing values)
+	> fill_in_method (optional, default 'fill_in_previous') --> which method to use to fill in the missing values at the initial stage. By default, this uses the most earliest known value (mod the length of the signal). This is the method for which we have theoretical results.
+(2) TestSignalRecovery.py: This creates 100 test signals of length 1000 to 2000 and randomly selects 5 to 10 values to be missing and tests the algorithm in SignalRecovery.py against a "stupid" algorithm which assumes the values are zero for the missing values. We can also test it against the fill in previous method. In the first case, we seem to win, in the second, we seem to lose. This might be a result of the types of time series here.
--- TestSignalRecovery.py
+++ TestSignalRecovery.py
@@ -1,33 +1,23 @@
 import numpy as np
 from scipy.fft import fft, ifft
 import copy
-from SignalRecovery import recover_signal
+from SignalRecovery import recover_signal, fill_in_previous, fill_in_zero
 
 def generate_test_signal(test_len):
     test_sig = [0] * test_len
     test_sig[0] = complex(2 * np.random.rand() - 1, 2 * np.random.rand() - 1)
     for j in range(1, test_len):
-        test_sig[j] = test_sig[j - 1] + 0.1 * complex(np.random.rand(), np.random.rand()) - complex(0.05, 0.05)
+        delta_t = 0.2 * complex(np.random.rand(), np.random.rand())
+        delta_t -= complex(0.1, 0.1)
+        test_sig[j] = test_sig[j - 1] + delta_t
     return(np.array(test_sig))
 
 print('j;err_norm;err_norm2;err_norm2/err_norm;PVE;PVE2;PVE/PVE2')
 for j in range(100):
-# for j in range(1):
     test_len = 1000 + np.random.choice(1001)
     n_missing = 5 + np.random.choice(6)
-    # test_len = 5
-    # n_missing = 1    
 
     test_signal = generate_test_signal(test_len)
-    # small_enough = False
-    # while small_enough is False:
-        # test_signal = 2 * np.random.rand(test_len) - 1
-        # test_signal = generate_test_signal(test_len)
-        # test_ffa = fft(test_signal)
-        # n_small = len([x for x in test_ffa if abs(x) < 0.05])
-        # print('{}/{}'.format(n_small, n_missing))
-        # if n_small >= n_missing:
-            # small_enough = True
 
     missing_spots = np.random.choice(test_len, n_missing, replace=False)
     test_signal_missing = copy.deepcopy(test_signal)
@@ -40,10 +30,17 @@ for j in range(100):
     err_norm = np.linalg.norm(err)
     PVE = 1 - err_norm/sig_norm
        
-    bad_est = copy.deepcopy(test_signal)
-    for x in missing_spots:
-        bad_est[x] = 0
+    bad_est = fill_in_zero(copy.deepcopy(test_signal_missing))
     err2 = np.array(test_signal) - np.array(bad_est)
     err_norm2 = np.linalg.norm(err2)
     PVE2 = 1 - err_norm2/sig_norm
-    print('{};{:2f};{:2f};{:2f};{:2f};{:2f};{:2f}'.format(j, err_norm, err_norm2, err_norm2/err_norm, PVE, PVE2, PVE/PVE2))
+    print('{};{:2f};{:2f};{:2f};{:2f};{:2f};{:2f}'.format(
+            j,
+            err_norm,
+            err_norm2,
+            err_norm2/err_norm,
+            PVE,
+            PVE2,
+            PVE/PVE2
+        )
+    )
